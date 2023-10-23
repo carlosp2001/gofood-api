5c4c95eb17b86232749859603fc6bb8fa79e04ee
Carlos Pineda <cara_p@unicah.edu>
2023-10-23 11:51:10 -0600

    Paquetes AWS SDK y Multer configurados

---
+++ utils/multer.js
@@ -0,0 +1,16 @@
+const multer =  require('multer');
+const path = require('path');
+
+// Almacenarlo en el disco
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, path.join(__dirname, '..', 'public', 'img')); // Ruta donde se guardarán los archivos
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+
+const storage = multer.memoryStorage();
+
+exports.upload = multer({storage})
